5a5487083b4e77eb0c1681ab116af92b1fcb6d0c
tkern0 <tkern9182@gmail.com>
2019-05-08 21:28:29 +1200

    Revert back to stable version
    equivalent to commit 5a6a59d

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -11,25 +11,11 @@ var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b4
 
 exports.getWeather = function(req, res) {
 	var city = req.query.city;
-    var lat = req.query.lat;
-    var lon = req.query.lon;
+	if( (city === null) || (typeof(city) === 'undefined') ) {
+		return res.status(400).send('city missing');
+	}
 
-    var aurl;
-    // Prefer using city when possible
-	if ((city === null) || (typeof(city) === 'undefined')) {
-        var noLat = (lat === null) || (typeof(lat) === 'undefined');
-        var noLon = (lon === null) || (typeof(lon) === 'undefined');
-        if (noLat && noLon) {
-            return res.status(400).send('no required paramaters present');
-        } else if (noLat) {
-            return res.status(400).send('longitude missing');
-        } else if (noLon) {
-            return res.status(400).send('latitude missing');
-        }
-        aurl = OPENWEATHERURL + '&lat=' + lat + '&lon=' + lon;
-	} else {
-        aurl = OPENWEATHERURL + '&q=' + city + ',nz';
-    }
+	var aurl = OPENWEATHERURL + '&q=' + city + ',nz';
 
 	request({
 		method: 'GET',
@@ -38,21 +24,51 @@ exports.getWeather = function(req, res) {
     }, function(err, resp, body) {
     	if(err) {
     		res.status(400).send('Failed to get the data');
+    		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
     			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
-    			var response = {
-                    city: body.name,
-                    weather: weath,
-                    coord: body.coord
-                };
+    			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
                 return res.status(400).send({msg:'Failed'});
             }
     	}
     });
+
 };
 router.get('/getWeather', exports.getWeather);
+
+
+exports.getWeather2 = function(req, res) {
+	var city = req.query.city;
+	if( (city === null) || (typeof(city) === 'undefined') ) {
+		return res.status(400).send('city missing');
+	}
+
+	var aurl = OPENWEATHERURL + '&q=' + city + ',nz';
+
+	request({
+		method: 'GET',
+        url: aurl,
+  		json: true
+    }, function(err, resp, body) {
+    	if(err) {
+    		res.status(400).send('Failed to get the data');
+    		//console.error("Failed to send request to openweathermap.org", err);
+    	} else {
+    		if(body.cod === 200) {
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
+    			var response = {city: body.name, weather: weath};
+    			return res.status(200).send(response);
+    		} else {
+                return res.status(400).send({msg:'Failed'});
+            }
+    	}
+    });
+
+};
+router.get('/getWeather2', exports.getWeather2);
+
 
 exports.router = router;
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,9 +1,5 @@
-// The map is more useful to use inside the ConsoleModule so this function acts as a quick passthrough for the google api
-function initMapPassThrough() {
-    angular.element(document.getElementById("body")).scope().initMap()
-}
 
-var ConsoleModule = angular.module('ConsoleModule', ['ngRoute', 'oc.lazyLoad']);
+var ConsoleModule = angular.module('ConsoleModule', ['ngRoute']);
 
 ConsoleModule.config(['$routeProvider', '$locationProvider','$sceDelegateProvider', '$httpProvider',
     function ($routeProvider, $locationProvider, $sceDelegateProvider, $httpProvider) {
@@ -14,53 +10,8 @@ ConsoleModule.config(['$routeProvider', '$locationProvider','$sceDelegateProvide
     });
 }]);
 
-ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$timeout', '$sce', '$oclazyLoad'
+ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$timeout', '$sce',
     function($scope, $http, $routeParams, $timeout, $sce) {
-
-    $ocLazyLoad.load('https://maps.googleapis.com/maps/api/js?key=AIzaSyBrafZjNoBogHKBca6JdCY280XALGdMzrU&callback=initMapPassThrough');
-    var map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -40.62, lng: 174.73},
-            zoom: 5
-        });
-        map.addListener('click', function(e) {
-            $http({
-                method: "GET",
-                url: '/api/v1/getWeather?lat=' + e.latLng.lat + "&lon=" + e.latLng.lng
-            }).then(parseResponse);
-        });
-    }
-
-    var markers = [null, null, null, null]
-    function parseResponse(response) {
-        if (response.data.msg == "Failed") {
-            return;
-        }
-
-        switch(which) {
-        case 1: {
-            $scope.city1Weather = response.data.weather;
-            break;
-        } case 2: {
-            $scope.city2Weather = response.data.weather;
-            break;
-        } case 3: {
-            $scope.city3Weather = response.data.weather;
-            break;
-        } case 4: {
-            $scope.city4Weather = response.data.weather;
-        }}
-
-        if (markers[which - 1] != null) {
-            markers[which - 1].setMap(null);
-        }
-        markers[which - 1] = new google.maps.Marker({
-            position: {lat: response.data.coord.lat, lng: response.data.coord.lon},
-            map: map,
-            title: response.data.city
-        });
-    }
 
     $scope.city = function(which) {
 
@@ -80,14 +31,28 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
             break;
         }}
 
-        if (data.length == 0) {
-            return;
-        }
-
         $http({
             method: "GET",
             url: '/api/v1/getWeather?city=' + data
-        }).then(parseResponse);
+        }).then(function(response) {
+            if (response.data.msg == "Failed") {
+                return;
+            }
+
+            switch(which) {
+            case 1: {
+                $scope.city1Weather = response.data.weather;
+                break;
+            } case 2: {
+                $scope.city2Weather = response.data.weather;
+                break;
+            } case 3: {
+                $scope.city3Weather = response.data.weather;
+                break;
+            } case 4: {
+                $scope.city4Weather = response.data.weather;
+            }}
+        });
     };
 
 }]);
