726dfb184590d94bf0d793a756ffd9ebc3c6089c
tkern0 <tkern9182@gmail.com>
2019-05-12 13:28:09 +1200

    auto update database
    also few bugfixes + cleanup

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,6 +1,6 @@
 // The map is more useful to use inside the ConsoleModule so this function acts as a quick passthrough for the google api
-function initMap() {
-    angular.element(document.getElementById("map")).scope().initMap();
+function init() {
+    angular.element(document.getElementById("map")).scope().init();
 }
 
 var ConsoleModule = angular.module('ConsoleModule', ['ngRoute']);
@@ -19,28 +19,33 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
 
     // Kind of hacky cause it doesn't want to work manually putting it into the base document
     var script = document.createElement('script');
-    script.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAzJa1V-QaHdETwp3QMo1EOFS8ly5b0y6o&callback=initMap');
+    script.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAzJa1V-QaHdETwp3QMo1EOFS8ly5b0y6o&callback=init');
     script.setAttribute('type', 'text/javascript');
     document.getElementById('map').appendChild(script);
 
-    cityNames = [
-        $scope.city1m,
-        $scope.city2m,
-        $scope.city3m,
-        $scope.city4m
-    ];
-    cityWeathers = [
-        $scope.city1Weather,
-        $scope.city2Weather,
-        $scope.city3Weather,
-        $scope.city4Weather
-    ];
     function setWeather(response, index) {
         if (response.data.msg == "Failed") {
             return;
         }
 
-        cityWeathers[index] = response.data.weather;
+        switch(index) {
+        case 0: {
+            $scope.city1m = response.data.city;
+            $scope.city1Weather = response.data.weather;
+            break;
+        } case 1: {
+            $scope.city2m = response.data.city;
+            $scope.city2Weather = response.data.weather;
+            break;
+        } case 2: {
+            $scope.city3m = response.data.city;
+            $scope.city3Weather = response.data.weather;
+            break;
+        } case 3: {
+            $scope.city4m = response.data.city;
+            $scope.city4Weather = response.data.weather;
+            break;
+        }}
 
         if (markers[index] != null) {
             markers[index].setMap(null);
@@ -50,13 +55,65 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
             map: map,
             title: response.data.city
         });
+
+        // This data should already be sanitised by the openweathermap api
+        runSQL(
+            "update dwb30208.data set name = " +
+            response.data.city, +
+            ", lat = " +
+            response.data.coord.lat +
+            ", lon = " +
+            response.data.coord.lon +
+            ", last = " +
+            response.data.weather +
+            " where index = " +
+            index
+        );
+    }
+
+    /*
+      Runs an SQL command on the d2 database and returns the results to the callback function
+      For some reason $http doesn't want to work through the CORS proxy properly, so we have to
+       use XMLHttpRequest()s instead
+    */
+    var base_url = "https://cors-anywhere.herokuapp.com/https://dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net/dbapi/v3";
+    var auth = ""; // This gets set in $scope.init()
+    function runSQL(command, callback) {
+        var xhr_query = new XMLHttpRequest();
+        xhr_query.open("POST", base_url + "/sql_jobs", true);
+        xhr_query.setRequestHeader('Content-type', 'application/json');
+        xhr_query.setRequestHeader('Authorization', auth);
+        xhr_query.onload = function() {
+            var xhr_result = new XMLHttpRequest();
+            xhr_result.open("GET", base_url + "/sql_jobs/" + JSON.parse(xhr_query.responseText).id, true);
+            xhr_result.setRequestHeader('Content-type', 'application/json');
+            xhr_result.setRequestHeader('Authorization', auth);
+            if (typeof callback == "function") {
+                xhr_result.onload = function() {
+                    callback(JSON.parse(xhr_result.responseText));
+                };
+            }
+            xhr_result.onerror = function() {
+                console.error("Error retrieving query results");
+                console.error(xhr_result);
+            };
+            xhr_result.send();
+        };
+        xhr_query.onerror = function() {
+            console.error("Error performing query");
+            console.error(xhr_query);
+        };
+        xhr_query.send(JSON.stringify({
+            "commands": command,
+            "limit": "4",
+            "separator": ";",
+            "stop_on_error": "no"
+        }));
     }
 
     var markers = [null, null, null, null];
     var map = null;
-    var auth = "";
-    $scope.initMap = function() {
-
+    $scope.init = function() {
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: -40.62, lng: 174.73},
             zoom: 5
@@ -71,12 +128,7 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
             });
         });
 
-        /*
-          Pull the default data from out db2 database
-          For some reason $http doesn't want to work through the cors proxy properly, so we have to
-           use XMLHttpRequest()s instead
-        */
-        var base_url = "https://cors-anywhere.herokuapp.com/https://dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net/dbapi/v3";
+        // Pull the default data from our db2 database
         var xhr_token = new XMLHttpRequest();
         xhr_token.open("POST", base_url + "/auth/tokens", true);
         xhr_token.setRequestHeader('Content-type', 'application/json');
@@ -84,74 +136,74 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
             var res_token = JSON.parse(xhr_token.responseText);
             auth = "Bearer " + res_token.token;
 
-            var xhr_query = new XMLHttpRequest();
-            xhr_query.open("POST", base_url + "/sql_jobs", true);
-            xhr_query.setRequestHeader('Content-type', 'application/json');
-            xhr_query.setRequestHeader('Authorization', auth);
-            xhr_query.onload = function() {
-                var res_query = JSON.parse(xhr_query.responseText);
+            runSQL("select * from dwb30208.data", function(data) {
+                // Get stored information
+                var rows = data.results[0].rows;
+                for (var i = 0; i < rows.length; i++) {
+                    var name = rows[i][1];
+                    var lat = rows[i][2];
+                    var lon = rows[i][3];
+                    var last = rows[i][4];
 
-                var xhr_data = new XMLHttpRequest();
-                xhr_data.open("GET", base_url + "/sql_jobs/" + res_query.id, true);
-                xhr_data.setRequestHeader('Content-type', 'application/json');
-                xhr_data.setRequestHeader('Authorization', auth);
-                xhr_data.onload = function() {
-                    var res_data = JSON.parse(xhr_data.responseText);
+                    switch(i) {
+                    case 0: {
+                        $scope.city1m = name;
+                        $scope.city1Weather = last;
+                        break;
+                    } case 1: {
+                        $scope.city2m = name;
+                        $scope.city2Weather = last;
+                        break;
+                    } case 2: {
+                        $scope.city3m = name;
+                        $scope.city3Weather = last;
+                        break;
+                    } case 3: {
+                        $scope.city4m = name;
+                        $scope.city4Weather = last;
+                        break;
+                    }}
 
-                    // Get stored information
-                    var rows = res_data.results[0].rows;
-                    for (var i = 0; i < rows.length; i++) {
-                        var name = rows[i][1];
-                        var lat = rows[i][2];
-                        var lon = rows[i][3];
-                        var last = rows[i][4];
-
-                        cityNames[i] = name;
-                        cityWeathers[i] = last;
-
-                        markers[i] = new google.maps.Marker({
-                            position: {lat: lat, lng: lon},
-                            map: map,
-                            title: name
-                        });
+                    // For testing, this function should only be called once so this should never go through
+                    if (markers[i] != null) {
+                        markers[i].setMap(null);
                     }
-                    /*
-                      Update information
-                      Can't put this in a loop cause it uses promises
-                    */
-                    $http({
-                        method: "GET",
-                        url: '/api/v1/getWeather?lat=' + rows[0][2] + "&lon=" + rows[0][3]
-                    }).then(function(response) {
-                        setWeather(response, 0);
+                    markers[i] = new google.maps.Marker({
+                        position: {lat: Number(lat), lng: Number(lon)},
+                        map: map,
+                        title: name
                     });
-                    $http({
-                        method: "GET",
-                        url: '/api/v1/getWeather?lat=' + rows[1][2] + "&lon=" + rows[1][3]
-                    }).then(function(response) {
-                        setWeather(response, 1);
-                    });
-                    $http({
-                        method: "GET",
-                        url: '/api/v1/getWeather?lat=' + rows[2][2] + "&lon=" + rows[2][3]
-                    }).then(function(response) {
-                        setWeather(response, 2);
-                    });
-                    $http({
-                        method: "GET",
-                        url: '/api/v1/getWeather?lat=' + rows[3][2] + "&lon=" + rows[3][3]
-                    }).then(function(response) {
-                        setWeather(response, 3);
-                    });
-                };
-                xhr_data.send();
-            };
-            xhr_query.send(JSON.stringify({
-                "commands":"select * from dwb30208.data",
-                "limit":"4",
-                "separator":";",
-                "stop_on_error":"no"
-            }));
+                }
+
+                /*
+                  Update information
+                  Can't put this in a loop cause it uses promises
+                */
+                $http({
+                    method: "GET",
+                    url: '/api/v1/getWeather?lat=' + rows[0][2] + "&lon=" + rows[0][3]
+                }).then(function(response) {
+                    setWeather(response, 0);
+                });
+                $http({
+                    method: "GET",
+                    url: '/api/v1/getWeather?lat=' + rows[1][2] + "&lon=" + rows[1][3]
+                }).then(function(response) {
+                    setWeather(response, 1);
+                });
+                $http({
+                    method: "GET",
+                    url: '/api/v1/getWeather?lat=' + rows[2][2] + "&lon=" + rows[2][3]
+                }).then(function(response) {
+                    setWeather(response, 2);
+                });
+                $http({
+                    method: "GET",
+                    url: '/api/v1/getWeather?lat=' + rows[3][2] + "&lon=" + rows[3][3]
+                }).then(function(response) {
+                    setWeather(response, 3);
+                });
+            });
         };
         // Not the best idea to put this out on github but I have no other way to get it into the app
         xhr_token.send(JSON.stringify({
@@ -161,9 +213,29 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     };
 
     $scope.city = function(which) {
+        var name = "";
+        switch(which) {
+        case 1: {
+            name = $scope.city1m;
+            break;
+        } case 2: {
+            name = $scope.city2m;
+            break;
+        } case 3: {
+            name = $scope.city3m;
+            break;
+        } case 4: {
+            name = $scope.city4m;
+            break;
+        }}
+
+        if (name.length == 0) {
+            return;
+        }
+
         $http({
             method: "GET",
-            url: '/api/v1/getWeather?city=' + cityNames[which - 1]
+            url: '/api/v1/getWeather?city=' + name
         }).then(function(response) {
             setWeather(response, which - 1);
         });
